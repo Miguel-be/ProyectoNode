1915bc9d708fff9dc56d36471de55738589fb1ff
Miguel-be <82940714+Miguel-be@users.noreply.github.com>
2023-01-20 11:58:40 +0100

    Update movies.routes.js

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -154,9 +154,47 @@ router.put("/edit/:id", [isAuthJWT], async(req,res,next)=>
     }
 })
 
+//End point duplicado para editar una pelicula. Se crea para el proyecto de React se quita el middleware de JWT pero se pone portada de 
+//pelicula
+router.put("/edit-free/:id", [upload.single("cover")], async(req,res,next)=>
+{
+    try {    
+            const cover= imagetoUri(filepath);
+            const {id}= req.params;
+            const movieChanges= new Movies({...req.body, cover});
+            //Guardamos datos en minúscula en BD para luego poder comparar fácilmente con minúsculas y no ser key sensitive
+            if (movieChanges.title) {movieChanges.title=movieChanges.title.toLowerCase();}
+            if (movieChanges.director){movieChanges.director=movieChanges.director.toLowerCase(); }
+            if (movieChanges.genre){movieChanges.genre=movieChanges.genre.toLowerCase();}           
+
+            movieChanges._id=id;                        
+            const movieToEdit= await Movies.findByIdAndUpdate(id,
+                {$set:{...movieChanges}},
+                {new:true}
+            );
+            await fs.unlinkSync(filepath);
+            res.status(201).json(movieToEdit);    
+    } catch (err) {
+        return next (err);
+    }
+})
+
 //End point para borrar una pelicula. Se incluye middleware para verificar que el usuario 
 //está registrado + login 
 router.delete("/delete/:id", [isAuthJWT], async(req,res,next)=>
+{
+    try {
+            const {id}= req.params;            
+            await Movies.remove({"_id": id}); 
+            res.status(201).json("Elemento eliminado correctamente");
+    } catch (err) {      
+        return next (err);
+    }
+})
+
+//End point duplicado para borrar una pelicula. Se quita middleware para verificar que el usuario 
+//está registrado + login 
+router.delete("/delete-free/:id", [], async(req,res,next)=>
 {
     try {
             const {id}= req.params;            
